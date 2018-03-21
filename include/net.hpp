d63f03dfa0680c44d9064507ce2e59891d6b8700
Felix Clark <mfclark3690@gmail.com>
2018-03-21 19:55:12 -0400

    backprop works and is tested for simple network

--- include/net.hpp
+++ include/net.hpp
@@ -3,7 +3,9 @@
 namespace { // protect these definitions locally, at least for now until we come up with a better global scheme
   template <size_t M, size_t N>
   using Mat = Eigen::Matrix<double, M, N>;
-
+  template <size_t M>
+  using Vec = Mat<M, 1>;
+  
   template <size_t M> // don't think we need anything but column arrays
   using Array = Eigen::Array<double, M, 1>;
 
@@ -11,7 +13,10 @@ namespace { // protect these definitions locally, at least for now until we come
 }
 
 template <typename Derived> // should work for scalars and eigen arrays
-Eigen::ArrayBase<Derived> sigmoid(const Eigen::Ref<const Derived>& in) {
+// Eigen::ArrayBase<Derived> sigmoid(const Eigen::Ref<const Derived>& in) {// Ref works for "both matrices and blocks", but is it quite what we want here?
+// Eigen::ArrayBase<Derived> sigmoid(const Eigen::ArrayBase<Derived>& in) { // this doesn't work?
+// see https://eigen.tuxfamily.org/dox/TopicFunctionTakingEigenTypes.html
+Eigen::ArrayBase<Derived> sigmoid(const Derived& in) { // i think this should work fine, but does require template argument to function
   using Eigen::exp;
   Derived result = 1.0/(1.0 + exp(-in));
   return result;
@@ -21,46 +26,180 @@ double sigmoid(double in) {
   return 1.0/(1.0 + exp(-in));
 }
 
-// N is input size, M is hidden layer size; single output function
+// maps sigma(x) to sigma'(x). note it is not a function of x, but sigma. this utilizes the expression
+template <typename Derived>
+Eigen::ArrayBase<Derived> sigmoid_to_d(const Derived& sig) { // i think this should work fine, but does require template argument to function
+  // sig = 1.0/(1.0 + exp(-in))
+  Derived result = sig*(1.0-sig);
+  return result;
+}
+
+// we will really want to break this apart (into layers?) and generalize it but first let's get a simple working example.
+// maybe this model should be called a "FeedForward" NN
+// this is a "single hidden layer feedforward network" (SLFN) with 1 output
+// specifically, right now it's an (exclusive) classifier which implies sigmoid/softmax on the output gate.
+// for a y-value of zero, it is taken to mean that the data represents none of the output classes.
+// for non-exclusive classification, independent NNs could be used.
+// another template argument could specify whether it's a classifier or least-squares, which should actually change surprisingly little code.
+// another template argument should indicate the internal activation functions:
+//    e.g. sigmoid (bad IRL), tanh, ReLU, softplus, etc. (softsign, arctan,...)
+// N is input size, M is hidden layer size, P is output size
 template <size_t N, size_t M>
-// should inherit from some neural network interface once we get that going
+// should inherit from some more general neural network interface once we get that going, as well as a template interface depending only on the number of input and output layers.
 class SingleHiddenLayer
 {
 private:
   static constexpr size_t size_w1_ = M*(N+1); // first layer
   static constexpr size_t size_w2_ = M+1; // output layer
-  static constexpr size_t size_ = size_w1_ + size_w2_; // total size of net
+  static constexpr size_t size_ = size_w1_ + size_w2_; // total size of data required to store net
 public:
   SingleHiddenLayer() {};
   ~SingleHiddenLayer() {};
+  constexpr static size_t size() {return size_;} // needs to be declared explicitly static to work
   void randomInit() {net_.setRandom();};
-  double getOutput(const Mat<N, 1>&) const; // should maybe take an array instead of a vector? idk
+  double getOutput() const; // returns cache
+  // maybe we should have a function like setInput() and otherwise don't allow new input arrays, so it's clear what changes the cache
+  // double doPropForward(const Vec<N>&); // should maybe take an array instead of a vector? idk
+  // Array<size_> doPropBackward(double); // our output right now has only 1 value // or return gradient?
+  void propagateData(const Vec<N>& x0, double y);
+  double getCostFuncVal() const;
+  const auto& getCostFuncGrad() const {return net_grad_cache_.eval();};
+  // there is a type (extra stage) of backprop that would give derivatives w.r.t. *inputs* (not net values).
+  // this could be useful for indicating which data elements are most relevant.
+  Array<M> getHiddenLayerActivation() const {return hidden_layer_cache_;};
   // auto getFirstSynapses() const {return A1_;};
-  // using "auto" may not be best when returning a map view -- TODO: look into this more.
+  // using "auto" may not be best when returning a map view -- TODO: look into this more?
   auto getFirstSynapses() const {return Map< const Mat<M, N+1> >(net_.data());};
   auto getSecondSynapses() const
   {return Map< const Mat<1, M+1> >(net_.template segment<size_w2_>(size_w1_).data());};
   // Array<size_> getNetValue(); // if we specify directly that it is of size size_, the compiler can not associate with the function definition properly. "auto" just works, apparently.
-  const auto& getNetValue() const {return net_;}
-  void setNetValue(const Array<size_>& in) {net_ = in;}
+  const auto& getNetValue() const {return net_.eval();}
+  // void setNetValue(const Eigen::Ref<const Array<size_>>& in) {net_ = in;}
+  auto& accessNetValue() {return net_;}
+  void resetCache();
 private:
+  // we could also store these as matrices and map to an array using segment<>
   Array<size_> net_ = Array<size_>::Zero();  
-
+  // we should also cache the activation layer values. this will be necessary for efficient backpropagation.
+  // possibly will need to do this for multiple input/output sets at once, for minibatch gradient descent,
+  // although this loop may be able to work externally
+  //  store the bias terms in the cache for convenience ?
+  Vec<N+1> input_layer_cache_ = Vec<N+1>::Identity(); // trick to set 1st element to 1 be default: bias term
+  Vec<M+1> hidden_layer_cache_ = Vec<M+1>::Identity();
+  double output_layer_cache_ = 0.;
+  Array<size_> net_grad_cache_ = Array<size_>::Zero(); // may not be necessary for this SLFN, but when we break up into layers it might be
+  double output_value_cache_ = 0.;
+  // possible other parameters: regularization; normalization of cost function
 };
 
+// this operation just resets all data to zero and so may not be particularly useful except perhaps for validation
+template <size_t N, size_t M>
+void SingleHiddenLayer<N,M>::resetCache() {
+  input_layer_cache_ = Vec<N+1>::Zero();
+  input_layer_cache_(0) = 1.; // bias term
+  hidden_layer_cache_ = Vec<M+1>::Zero();
+  hidden_layer_cache_(0) = 1.; // bias term
+  output_layer_cache_ = 0.;
+  output_value_cache_ = 0.;
+  net_grad_cache_ = Array<size_>::Zero();
+}
 
 // simply return the value of the network for a given input. we'll use a simple sigmoid activation function for now (make this adjustable later)
 template <size_t N, size_t M>
-double SingleHiddenLayer<N,M>::getOutput(const Mat<N, 1>& x0) const {
-  // at some point we might wish to experiment with column-major (default) vs. row-major data storage order
-  const Mat<M, N+1>& w1 = getFirstSynapses();
-  const Mat<1, M+1>& w2 = getSecondSynapses();
-  const Mat<M, 1> a1 = w1.template leftCols<1>() // bias term
-    + w1.template rightCols<N>() * x0; // matrix mult term
-  const Mat<M, 1> x1 = sigmoid< Array<M> >(a1.array()).matrix(); // apply activation function element-wise; need to specify the template type
-  Mat<1,1> a2 = w2.template leftCols<1>()
-    + w2.template rightCols<M>() * x1;
-
-  return sigmoid(a2(0,0)); // this version is outputting a scalar; just access it directly
+double SingleHiddenLayer<N,M>::getOutput() const {
+  return output_layer_cache_;
 }
 
+// we should be able to build up multi-layer networks by having each layer work as its own and having their forward and backward propagations interact with each other
+// this function performs both the forward then the backward propagation for a given data point
+// generalization to mini-batches might be able to occur outside
+
+// should really consider changing the interface to have the parameters be external, but this is probably only useful for validation;
+//  for real training, it will help to have the gradient stored (except perhaps accelerated gradient)
+template <size_t N, size_t M>
+void SingleHiddenLayer<N,M>::propagateData(const Vec<N>& x0, double y) {
+  // resetCache(); // ? shouldn't actually be necessary; should compare in checks
+  
+  // propagate forwards
+  Mat<M, N+1> w1 = getFirstSynapses(); // don't think there is copying here for a Map object... but we should verify how it actually works.
+  Mat<1, M+1> w2 = getSecondSynapses(); // const references shouldn't be necessary for the expression templates to work
+
+  // input_layer_cache_(0) = 1.;
+  // hidden_layer_cache_(0) = 1.;
+  assert( input_layer_cache_(0) == 1. && hidden_layer_cache_(0) == 1.);
+  input_layer_cache_.template segment<N>(1) = x0; // offset bias term
+  // at some point we might wish to experiment with column-major (default) vs. row-major data storage order
+  // Vec<M> a1 = w1.template leftCols<1>() // bias term
+  //   + w1.template rightCols<N>() * input_layer_cache; // matrix mult term
+  Vec<M> a1 = w1 * input_layer_cache_; // matrix mult term
+  // apply activation function element-wise; need to specify the template type
+  hidden_layer_cache_.template segment<M>(1) = sigmoid< Array<M> >(a1.array()).matrix().eval();  // offset with bias term
+  // Vec<1> a2 = w2.template leftCols<1>()
+  //   + w2.template rightCols<M>() * hidden_layer_cache_;
+  Vec<1> a2 = w2 * hidden_layer_cache_;
+  output_layer_cache_ = sigmoid(a2(0,0))/*eval*/; // this is sort-of implicit logistic regression; 1D version of softmax
+  // return output_layer_cache_; // this version is outputting a scalar; just access it directly
+  output_value_cache_ = y;
+
+  // now do backwards propagation
+  // the dependence of the cost function on the data y depends on the type of regression, though it is quite similar for logistic and linear.
+  // we will assume logistic for now, but it should require minor changes to generalize.
+
+  // there is usually a 1/N term for the number of data points as well; perhaps should be handled externally
+  // or maybe this should be 1/N(output layers), since a 2-output probability net should be the same as a 1-output one
+  
+  /// note that this term has exact cancellation with 1st term of backprop w/ sigmoid activation
+  // , since 1st term is sigma'(a^2) = sigma*(1-sigma)
+  // it probably makes sense to use a sigmoid activation on the final layer (or softmax for multi-dimensional) regardless of internal activation functions
+  // double logistic_term = (output_layer_cache_ - y)/(output_layer_cache_*(1-output_layer_cache_)); // the denominator cancels out of sigmoid/softmax
+  // there are cross-terms for softmax if the cost function is the sum of that in all nodes. check this for the multidimensional case! -- yes, the cancellation works just the same.
+  // for the multidimensional case, should there be an implied additional case for noise? (i.e. for which y=0 in all bins) it probably won't affect logistic regression, but the cost function is different.
+  // this might actually have the same cancelation in least-square...?
+  
+  // the other term in the product is dx^out/d(p), which has a layer-dependent form
+    // derivative of softmax sm(x) is dsm(a_j)/d(a_k) = sm(a_j)*[delta_jk - sm(a_k)]
+  // we actually will want a softmax with a non-interacting extra input layer w/ value 0 for normalization (for non-classified, or y=0)
+
+  Vec<1> delta_out;// = output_layer_cache_ - y;
+  delta_out << (output_layer_cache_ - y);
+  // the above is a vector of length (output size) that must be contracted with the LHS of these matrices
+  // it turns out we can contract this vector pretty early on
+  
+  // with layer L being the output layer:
+  // dx_j^(L)/dw_mn^(L) = sigma'[a_j^(L)] * delta_jm * x_n^(L-1)
+  // dx_j^(L)/dw_mn^(L-1) = sigma'[a_j^(L)] * w_jm^(L) * sigma'[a_m^(L-1)] * x_n^(L-2)
+  // dx_j^(L)/dw_mn^(L-2) = sigma'[a_j^(L)] * Sum_k{  w_jk^(L) * sigma'[a_k^(L-1)] * w_km^(L-1) } * sigma'[a_m^(L-2)] * x_n^(L-3)
+  /// more generally:
+  // dx_j^(L)/dw_mn^(L') = O^(L')_jm * x^(L')_n    where O^(L)_jm = delta_jm and
+  //   O^(L-1)_jm = O^(L)_jk * w^(L)_km sigma'[a^(L)_m]  (matrix multiplication)
+  
+  Vec<1> o2 = Mat<1, 1>::Identity() * delta_out;
+  
+  Vec<M> d1 = sigmoid_to_d<Array<M>>(hidden_layer_cache_.template segment<M>(1).array()).matrix(); // ignores bias term explicitly
+  Vec<M> o1 = d1.asDiagonal() * (w2.template rightCols<M>()).transpose() * o2;
+
+
+  auto grad_w2 = Map< Mat<1, M+1> >(net_grad_cache_.template segment<size_w2_>(size_w1_).data());
+  auto grad_w1 = Map< Mat<M, N+1> >(net_grad_cache_.template segment<size_w1_>(0).data());
+
+  grad_w2 = o2 * hidden_layer_cache_.transpose();
+  grad_w1 = o1 * input_layer_cache_.transpose();
+  
+}
+
+// define these here for now; they may stay this simple but they also may not.
+// should cache outputs and then template-specialize this based on the settings (i.e. classifier vs. least-squares)
+template <size_t N, size_t M>
+double SingleHiddenLayer<N,M>::getCostFuncVal() const {
+  /// - (output_value_cache_.array()*log(output_layer_cache_.array())).sum(); // multi-dimensional case
+  // , but the multi-D case might need a term that accounts for a non-classified value.
+  // maybe we add this in by hand? i.e. another y component equal to 1-sum(y_i) ?
+  return  - output_value_cache_*log(output_layer_cache_)
+    - (1-output_value_cache_)*log(1-output_layer_cache_); // 1-dimensional case
+}
+
+// template <size_t N, size_t M>
+// // would be nice to be able to write Array<size_> in the return signature
+// Array<M*(N+1)+(M+1)> SingleHiddenLayer<N,M>::getCostFuncGrad() const {
+//   return net_grad_cache_;
+// }
--- src/test_net.cpp
+++ src/test_net.cpp
@@ -3,27 +3,122 @@
 #include <Eigen/Core>
 #include <iostream>
 
+#include <functional>
+
 using std::cout;
 using std::endl;
 
+template <size_t N>
+using parvec = Eigen::Array<double, N, 1>;
+
+template <size_t N>
+using func_t = std::function<double(parvec<N>)>;
+template <size_t N>
+using grad_t = std::function<parvec<N>(parvec<N>)>;
+
+// template <size_t N>
+// using Vec = Eigen::Matrix<double, N, 1>;
+
+// borrow this function from other testing utility to check the neural net's derivative
+// we will really want to check element-by-element
+// template <size_t N>
+// void check_gradient(func_t<N> f, grad_t<N> g, const parvec<N>& p, double ep=1e-6, double tol=1e-2) {
+void check_gradient(SingleHiddenLayer<8, 4>& net, const Vec<8>& xin, const double yin, double ep=1e-4, double tol=1e-4) {
+  // assume that a data point has already been put in
+  constexpr size_t N = net.size();
+  parvec<N> p = net.getNetValue(); // don't make this a reference: the internal data will change!
+
+  net.propagateData(xin, yin); // this must be done before 
+  double fval = net.getCostFuncVal(); // don't think we actually need this, but it might be a nice check
+
+  parvec<N> evalgrad = net.getCostFuncGrad();
+  double gradmag = sqrt(evalgrad.square().sum());
+  parvec<N> dir = (1.0+p.square()).sqrt()*evalgrad/gradmag; // vector along direction of greatest change
+  double dirmag = sqrt(dir.square().sum());
+  parvec<N> dpar = ep*dir;
+
+  net.accessNetValue() += dpar;
+  net.propagateData(xin, yin);
+  double fplus = net.getCostFuncVal();
+
+  net.accessNetValue() = p-dpar;
+  net.propagateData(xin, yin);
+  double fminus = net.getCostFuncVal();
+
+
+  parvec<N> df;// maybe can do this slickly with colwise() or rowwise() ?
+  for (size_t i_f=0; i_f<N; ++i_f) {
+    parvec<N> dp = Array<N>::Zero();
+    double dpi = ep*sqrt(1.0 + p(i_f)*p(i_f));
+    dp(i_f) = dpi;
+    parvec<N> pplus = p + dp;
+    parvec<N> pminus = p - dp;
+    net.accessNetValue() = pplus;
+    net.propagateData(xin, yin);
+    double fplusi = net.getCostFuncVal();
+    net.accessNetValue() = pminus;
+    net.propagateData(xin, yin);
+    double fminusi = net.getCostFuncVal();
+    df(i_f) = (fplusi - fminusi)/(2*dpi);
+  }
+
+  // now check this element-by element
+  if ( (df-evalgrad).square().sum() > tol*tol*1
+       || !df.isFinite().all()
+       || !evalgrad.isFinite().all()) {
+    cout << "gradient check failed!" << endl;
+    cout << "f(p) = " << fval << endl;
+    cout << "numerical derivative = " << df << endl;
+    cout << "analytic gradient = " << evalgrad << endl;
+    cout << "difference = " << df - evalgrad << endl;
+  }
+  
+  // we should actually check the numerical derivative element-by-element
+  // double deltaf = (f(p+ep*dir) - f(p-ep*dir))/(2*ep*dirmag); // should be close to |gradf|
+  double deltaf = (fplus - fminus)/(2*ep*dirmag);
+  if ( fabs(deltaf) - (dir*evalgrad).sum()/dirmag > tol*fabs(deltaf + (dir*evalgrad).sum()/dirmag) ) {
+    cout << "gradient magnitude check failed!" << endl;
+    cout << "f(p) = " << fval << endl;
+    cout << "numerical derivative along grad = " << deltaf << endl;
+    cout << "analytic gradient magnitude = " << (dir*evalgrad).sum()/dirmag << endl;
+    cout << "difference = " << fabs(deltaf) - (dir*evalgrad).sum()/dirmag << endl;
+  }
+}
+
 int main(int argc, char** argv) {
 
-  SingleHiddenLayer<8, 4> testNet;
+  SingleHiddenLayer<8, 4/*, 2*/> testNet;
+  constexpr size_t netsize = testNet.size();
   testNet.randomInit();
 
-  Eigen::Matrix<double, 8, 1> input;
+  Vec<8> input;
   input.setRandom();
 
+  Vec<2> output;
+  output << 0, 1;
+  double yval = 0.5;// testing single-dimension for now
+  
   Eigen::IOFormat my_fmt(2, // first value is the precision
 			 0, ", ", "\n", "[", "]");
-  
-  cout << "output of random network is:  " << testNet.getOutput(input) << endl;
+
+  testNet.propagateData( input, yval/*output*/ );
+  cout << "output of random network is:  " << testNet.getOutput() << endl;
   cout << "first layer:\n" << testNet.getFirstSynapses().format(my_fmt) << endl;
   cout << "second layer:\n" << testNet.getSecondSynapses().format(my_fmt) << endl;
   auto& pars = testNet.getNetValue();
   // cout << "net value of array:\n" << testNet.getNetValue() << endl;
   cout << "array has " << pars.size() << " parameters." << endl;
-  
+
+  check_gradient( testNet, input, yval/*output*/ );
+
+  input.setRandom();
+  yval = 0.02;
+  check_gradient( testNet, input, yval );
+
+  input.setRandom();
+  yval = 0.99;
+  check_gradient( testNet, input, yval );
   
   return 0;
 }
+
