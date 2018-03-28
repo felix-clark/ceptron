809381ec155e750af78b455c1f2f47bed7047bdd
Felix Clark <mfclark3690@gmail.com>
2018-03-28 13:48:15 -0400

    changing some default typedefs to be dynamic

--- include/global.hpp
+++ include/global.hpp
@@ -3,26 +3,26 @@
 
 namespace ceptron {
 
-  template <size_t M, size_t N>
+  template <int M, int N>
   using Mat = Eigen::Matrix<double, M, N>;
-  template <size_t M>
+  template <int M>
   using Vec = Mat<M, 1>;
-  template <size_t M>
+  template <int M>
   using BatchVec = Eigen::Matrix<double, M, Eigen::Dynamic>;
   
-  template <size_t M> // don't think we need anything but column arrays
+  template <int M> // don't think we need anything but column arrays
   using Array = Eigen::Array<double, M, 1>;
-  template <size_t M> // we might use this type explicitly for element-wise computations
+  template <int M> // we might use this type explicitly for element-wise computations
   using BatchArray = Eigen::Array<double, M, Eigen::Dynamic>;
 
   using Eigen::Map;
 
   // struct for returning a function value with its gradient.
   // often we are only interested in the gradient
-  template <size_t M>
+  template <int M=Eigen::Dynamic>
   struct func_grad_res {
     double f=0.;
-    Array<M> g = Array<M>::Zero();
+    Array<M> g = Array<M>::Zero(M);
   };
   
 } // namespace ceptron
