11d55345a686c9845265db7cfc710e80191f132e
Felix Clark <mfclark3690@gmail.com>
2018-04-01 09:51:42 -0400

    added dynamic-sized typedefs for arrays

--- include/global.hpp
+++ include/global.hpp
@@ -5,15 +5,19 @@ namespace ceptron {
 
   template <int M, int N>
   using Mat = Eigen::Matrix<double, M, N>;
+  using MatX = Mat<Eigen::Dynamic, Eigen::Dynamic>;
   template <int M=Eigen::Dynamic>
   using Vec = Mat<M, 1>;
+  using VecX = Vec<Eigen::Dynamic>;
   template <int M=Eigen::Dynamic>
   using BatchVec = Eigen::Matrix<double, M, Eigen::Dynamic>;
   
   template <int M=Eigen::Dynamic> // don't think we need anything but column arrays
   using Array = Eigen::Array<double, M, 1>;
+  using ArrayX = Array<Eigen::Dynamic>;
   template <int M=Eigen::Dynamic> // we might use this type explicitly for element-wise computations
   using BatchArray = Eigen::Array<double, M, Eigen::Dynamic>;
+  using BatchArrayX = BatchArray<Eigen::Dynamic>;
 
   using Eigen::Map;
 
