1fe78d1e15fe4d08e97a53f6e4c2fb9da2943049
Felix Clark <mfclark3690@gmail.com>
2018-04-04 07:33:43 -0400

    moving dynamic nets into namespace; changed layer to private member class

--- include/net_dyn.hpp
+++ include/net_dyn.hpp
@@ -8,96 +8,91 @@
 #include <vector>
 #include <memory>
 
-namespace {
-  using namespace ceptron;
-  
-} // namespace
+// namespace {
+//   using namespace ceptron;  
+// } // namespace
 
-class Layer;
-// class OutputLayer;
+namespace ceptron {
+  // a runtime-sized feedforward network
+  // not sure  yet that we actually need a separate class for this
+  class FfnDyn
+  {
+  public:
+    template <typename...Ts> FfnDyn(RegressionType reg, InternalActivator act, Ts... layer_sizes);
+    ~FfnDyn() = default;
+    FfnDyn(const FfnDyn&) = delete; // we do some non-trivial memory operations w/ shared_ptrs so don't allow copying (not sure if it works)
+    // FfnDyn(FfnDyn&&) = delete; // move ops are not defined when we implement or delete the copy-constructor
+    // might need to disable operator= too?
+    int num_weights() const {return size_;} // number of weights in net (size of gradient)
+    size_t getNumOutputs() const;
+    double costFunc(const Eigen::Ref<const ArrayX>& netvals, const BatchArrayX& xin, const BatchArrayX& yin) const;
+  private:
+    class Layer;// we need to forward-declare the class
+    // std::vector<Layer> layers_; // and perhaps we need only point to the first layer
+    // these pointers could probably be unique for simple feed-forward nets
+    std::shared_ptr<Layer> first_layer_;
+    size_t size_; // saving this may be redundant, but useful for quick assertions
 
-// a runtime-sized feedforward network
-// not sure  yet that we actually need a separate class for this
-class FfnDyn
-{
-public:
-  template <typename...Ts> FfnDyn(RegressionType reg, InternalActivator act, Ts... layer_sizes);
-  ~FfnDyn() = default;
-  FfnDyn(const FfnDyn&) = delete; // we do some non-trivial memory operations w/ shared_ptrs so don't allow copying (not sure if it works)
-  // FfnDyn(FfnDyn&&) = delete; // move ops are not defined when we implement or delete the copy-constructor
-  // might need to disable operator= too?
-  int num_weights() const {return size_;} // number of weights in net (size of gradient)
-  size_t getNumOutputs() const;
-  double costFunc(const Eigen::Ref<const ArrayX>& netvals, const BatchArrayX& xin, const BatchArrayX& yin) const;
-private:
-  // ArrayX net_; // we probably shouldn't save data internally.
-  // std::vector<Layer> layers_; // and perhaps we need only point to the first layer
-  // these pointers could probably be unique for simple feed-forward nets
-  std::shared_ptr<Layer> first_layer_;
-  size_t size_; // saving this may be redundant, but useful for quick assertions
-};
+    // a "Layer" here refers to the structure that includes the weight matrix taking "inputs_" values and mapping to "outputs_" values, with an activation function on top.
+    class Layer
+    {
+    public:
+      // constructor for output layer
+      Layer(InternalActivator act, RegressionType reg, size_t ins, size_t outs);
+      // recursive constructor for internal layers
+      template <typename ...Ts> Layer(InternalActivator act, RegressionType reg, size_t ins, size_t n1, size_t n2, Ts... sizes);
+      virtual ~Layer() = default;
+    
+      double getCostFunction(const Eigen::Ref<const ArrayX>& net, const BatchArrayX& xin, const BatchArrayX& yin) const;
+    
+      size_t getNumInputs() const {return inputs_;}
+      size_t getNumOutputs() const {return outputs_;}
+      size_t getNumEndOutputs() const;
+      int getNumWeights() const {return outputs_*(inputs_+1);}
+      size_t getNumWeightsForward() const; // recursively compute the total number of weight elements including this layer and all in front
+    protected:
+      size_t inputs_;
+      size_t outputs_;
+      size_t num_weights_;
+    private:
+      // the output layer won't need to access these. in fact they will be somewhat wasted memory.
+      // if it was significant we could split this into InputLayer, but that's probably not a big deal.
+    
+      bool is_output_ = false; // is this the last layer in the net?
+      InternalActivator act_;
+      RegressionType reg_; // it's redundant saving this in every layer but it makes things easier for now.
+      // we could do a dynamic check on sizeof...(others) to specialize Layer w/ a separate OutputLayer class
+      std::shared_ptr<Layer> next_layer_;
+      // is a pointer to the last layer useful for anything? seems we can get most everything recursively
+    }; // class Layer
 
-// a "Layer" here refers to the structure that includes the weight matrix taking "inputs_" values and mapping to "outputs_" values, with an activation function on top.
-// these layers should possibly all be non-exposed classes used only by the network
-class Layer
-{
-public:
-   // constructor for output layer
-  Layer(InternalActivator act, RegressionType reg, size_t ins, size_t outs);
-  // recursive constructor for internal layers
-  template <typename ...Ts> Layer(InternalActivator act, RegressionType reg, size_t ins, size_t n1, size_t n2, Ts... sizes);
-  virtual ~Layer() = default;
-
-  double getCostFunction(const Eigen::Ref<const ArrayX>& net, const BatchArrayX& xin, const BatchArrayX& yin) const;
-  
-  size_t getNumInputs() const {return inputs_;}
-  size_t getNumOutputs() const {return outputs_;}
-  size_t getNumEndOutputs() const;
-  int getNumWeights() const {return outputs_*(inputs_+1);}
-  size_t getNumWeightsForward() const; // recursively compute the total number of weight elements including this layer and all in front
-protected:
-  size_t inputs_;
-  size_t outputs_;
-private:
-  // the output layer won't need to access these. in fact they will be somewhat wasted memory.
-  // if it was significant we could split this into InputLayer, but that's probably not a big deal.
-  
-  bool is_output_ = false; // is this the last layer in the net?
-  // Map<VecX> bias_;// map values can be passed in at call time; don't save as data
-  // Map<MatX> weights_;
-  InternalActivator act_;
-  RegressionType reg_; // it's redundant saving this in every layer but it makes things easier for now.
-  // we could do a dynamic check on sizeof...(others) to specialize Layer w/ a separate OutputLayer class
-  std::shared_ptr<Layer> next_layer_;
-  // is a pointer to the last layer useful for anything? seems we can get most everything recursively
-};
+  }; // class FfnDyn
 
 
-template <typename... Ts> FfnDyn::FfnDyn(RegressionType reg, InternalActivator act, Ts... layer_sizes)
-{
-  static_assert( sizeof...(layer_sizes) > 1, "network needs more than a single layer to be meaningful" );
-  first_layer_ = std::make_shared<Layer>(act, reg, layer_sizes...);
-  size_ = first_layer_->getNumWeightsForward();
-}
+  template <typename... Ts> FfnDyn::FfnDyn(RegressionType reg, InternalActivator act, Ts... layer_sizes)
+  {
+    static_assert( sizeof...(layer_sizes) > 1, "network needs more than a single layer to be meaningful" );
+    first_layer_ = std::make_shared<Layer>(act, reg, layer_sizes...);
+    size_ = first_layer_->getNumWeightsForward();
+  }
 
 
-// could maybe switch order of parameters w/ a template, but it's probably not worth it
-template <typename...Ts>
-Layer::Layer(InternalActivator act, RegressionType reg,
-	     size_t ins, size_t n1, size_t n2,
-	     Ts... others)
-  : inputs_(ins)
-  , is_output_(false)
-  // , bias_(data.data(), outs)
-  // , weights_(data.segment(outs, ins*outs).data(), outs, ins)
-  , act_(act)
-  , reg_(reg)
-{
-  // static_assert( sizeof...(Ts) > 0, "this should only get called for intermediate layers" );
-  next_layer_ = std::make_shared<Layer>(act, reg, n1, n2, others...);
-  outputs_ = n1;
-  // outputs_ = next_layer_->getNumInputs(); // this should give us the first element of others...
-	  
-  BOOST_LOG_TRIVIAL(debug) << "in intermediate layer constructor with " << inputs_ << " inputs and " << outputs_ << " outputs.";
-}
+  // could maybe switch order of parameters w/ a template, but it's probably not worth it
+  template <typename...Ts>
+  FfnDyn::Layer::Layer(InternalActivator act, RegressionType reg,
+		       size_t ins, size_t n1, size_t n2,
+		       Ts... others)
+    : inputs_(ins)
+    , is_output_(false)
+    , act_(act)
+    , reg_(reg)
+  {
+    // static_assert( sizeof...(Ts) > 0, "this should only get called for intermediate layers" ); // this assert is no longer necessary
+    next_layer_ = std::make_shared<Layer>(act, reg, n1, n2, others...);
+    outputs_ = n1;
+    // outputs_ = next_layer_->getNumInputs(); // this would also give us the first element of others...
+    num_weights_ = outputs_*(inputs_+1);
+    BOOST_LOG_TRIVIAL(trace) << "in intermediate layer constructor with " << inputs_ << " inputs and " << outputs_ << " outputs.";
+  }
 
+} // namespace ceptron
--- src/net_dyn.cpp
+++ src/net_dyn.cpp
@@ -17,47 +17,43 @@ double FfnDyn::costFunc(const Eigen::Ref<const ArrayX>& netvals, const BatchArra
 
 // ---- Layer ----
 
-// template <>
-Layer::Layer(InternalActivator act, RegressionType reg,
+FfnDyn::Layer::Layer(InternalActivator act, RegressionType reg,
 	     size_t ins, size_t outs)
   : inputs_(ins)
   , outputs_(outs)
+  , num_weights_(outputs_*(inputs_+1))
   , is_output_(true)
-  // , bias_(data.data(), outs)
-  // , weights_(data.segment(outs, ins*outs).data(), outs, ins)
   , act_(act)
   , reg_(reg)
 {
-  // do something with reg to save it. possibly point to appropriate functions.
-  BOOST_LOG_TRIVIAL(debug) << "in output layer constructor with " << ins << " inputs and " << outs << " outputs.";
+  BOOST_LOG_TRIVIAL(trace) << "in output layer constructor with " << ins << " inputs and " << outs << " outputs.";
 }
 
-size_t Layer::getNumWeightsForward() const {
+size_t FfnDyn::Layer::getNumWeightsForward() const {
   const size_t thisSize = getNumWeights();
   return is_output_ ? thisSize : thisSize + next_layer_->getNumWeightsForward();
 }
 
-size_t Layer::getNumEndOutputs() const {
+size_t FfnDyn::Layer::getNumEndOutputs() const {
   return is_output_ ? outputs_ : next_layer_->getNumEndOutputs();
 }
 
-double Layer::getCostFunction(const Eigen::Ref<const ArrayX>& netvals, const BatchArrayX& xin, const BatchArrayX& yin) const { // , regularization etc.)
-  const auto batchsize = xin.cols();
-  // could assert that xin.cols() == yin.cols(). maybe in gradient version
-  BatchVecX bias =
-    Eigen::Map<const VecX>(netvals.segment(0,outputs_).data(), outputs_, 1)
-    * BatchVecX::Ones(1,batchsize);
-  MatX weights = Eigen::Map<const MatX>(netvals.segment(outputs_, inputs_*outputs_).data(), outputs_, inputs_);
-  const int thissize = outputs_*(inputs_+1); // this could be computed/saved ahead of time, like at construction
+double FfnDyn::Layer::getCostFunction(const Eigen::Ref<const ArrayX>& netvals, const BatchArrayX& xin, const BatchArrayX& yin) const { // , regularization etc.)
+  // const auto batchsize = xin.cols();
+  // could assert that xin.cols() == yin.cols(). maybe just in slower gradient version
+  VecX bias = Eigen::Map<const VecX>(netvals.segment(0,outputs_).data(), outputs_, 1);
+  MatX weights = Eigen::Map<const MatX>(netvals.segment(outputs_, inputs_*outputs_).data(),
+					outputs_, inputs_);
   const auto insize = netvals.size();
-  BatchVecX x1 = weights*xin.matrix() + bias;
+  BatchVecX x1 = weights*xin.matrix();// + bias;
+  x1.colwise() += bias;
   if (is_output_) {
     assert( insize == getNumWeights() );
-    return costFuncVal(reg_, xin, yin);
+    return costFuncVal(reg_, x1, yin);
   } else {
     assert( insize > getNumWeights() );
     return next_layer_->
-      getCostFunction( netvals.segment(thissize, insize-thissize),
+      getCostFunction( netvals.segment(num_weights_, insize-num_weights_),
 		       activ(act_, x1.array()), yin );
   }
 }
